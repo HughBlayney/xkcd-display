cecf70a156c58b231d0677b7b54314284053b46d
Conrad Godfrey <conradjgodfrey@gmail.com>
2022-10-22 17:15:12 +0100

    add stuff

---
+++ get_latest.py
@@ -0,0 +1,30 @@
+import json
+
+import requests
+
+
+result = requests.get("https://xkcd.com/info.0.json")
+
+"""
+{
+  "month": "10",
+  "num": 2688,
+  "link": "",
+  "year": "2022",
+  "news": "",
+  "safe_title": "Bubble Universes",
+  "transcript": "",
+  "alt": "The theory finally unifies cosmic inflation and regular inflation.",
+  "img": "https://imgs.xkcd.com/comics/bubble_universes.png",
+  "title": "Bubble Universes",
+  "day": "21"
+}
+"""
+
+result_json = json.loads(result.text)
+
+result = requests.get(result_json["img"])
+
+with open("comic.png", "wb") as f:
+    f.write(result.content)
+
