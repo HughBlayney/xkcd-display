58c93cd57306bc279004c21334a9bb801478f06e
Conrad Godfrey <conradjgodfrey@gmail.com>
2023-08-14 21:23:42 +0100

    fix

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 function flip() {
     const button = document.querySelector('.button');
     button.classList.add('flipping');
-    fetch("{{ ngrok_url }}/flip", {
+    fetch(NGROK_URL + "/flip", {
         method: "GET"
     }).then(() => {
         button.classList.remove('flipping');
