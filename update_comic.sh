429d31a161bf25f3e03a3858459f2b16a8e88e03
conrad <conradjgodfrey@gmail.com>
2023-08-14 20:54:14 +0100

    Update update comic

--- update_comic.sh
+++ update_comic.sh
@@ -1,3 +1,3 @@
-python get_latest.py
+python download_comic.py
 convert comic.png -resize 800x480 -background white -gravity center -extent 800x480 resized.png
-python display_image.py
+python display.py --comic
