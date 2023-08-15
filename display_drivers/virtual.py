57ea7a2e7d2fd27ce150f3f27c29b1dfd7b794c1
Conrad Godfrey <conrad.godfrey@joinzoe.com>
2023-08-15 22:05:28 +0100

    improve visuals

--- display_drivers/virtual.py
+++ display_drivers/virtual.py
@@ -1,5 +1,4 @@
-from random import random
-
+from PIL import Image
 from decouple import config
 
 from display_drivers.abstract import AbstractDisplayDriver, EPD480x800
@@ -9,15 +8,17 @@ IMAGE_PATH = config("XKCD_VIRTUAL_IMAGE_PATH")
 
 
 class VirtualDisplayDriver(AbstractDisplayDriver, EPD480x800):
+    path = IMAGE_PATH
+
+    def display(self, image: Image):
+        image.save(self.path)
 
     def display_image(self, image_path: str = "./resized.png"):
+        image = Image.open(image_path)
+        self.display(image)
         print("displaying image at path: " + image_path)
 
     def display_text(self, text: str):
-        text = f"hello world foo + {random()}"
-
         image = XKCDAltTextRenderer(width=self.width, height=self.height).render(text)
-        # Save the image
-        image.save(IMAGE_PATH)
-
+        self.display(image)
         print("displaying text at path: " + text)
