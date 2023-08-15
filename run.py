6a7e083f320a18ed4ce5d7296490c298d44b5f0f
Conrad Godfrey <conrad.godfrey@joinzoe.com>
2023-08-16 00:02:33 +0100

    refactor to remove conditionals

--- run.py
+++ run.py
@@ -4,36 +4,42 @@ import subprocess
 from display_drivers.eink import EInkDisplayDriver
 from download_comic import download_comic, get_comic_info_json
 
+
+class XkcdClient:
+    def download_latest(self):
+        try:
+            with open("meta.json") as f:
+                meta = json.load(f)
+            displayed_comic_number = meta.get("num", None)
+        except FileNotFoundError:
+            displayed_comic_number = None
+
+        latest_comic_info_json = get_comic_info_json()
+
+        latest_comic_number = latest_comic_info_json.get("num", None)
+
+        if latest_comic_number is None or latest_comic_number == displayed_comic_number:
+            print("No new comic to display")
+        else:
+            download_comic()
+            # Run convert comic.png -resize 800x480 -background white -gravity center -extent 800x480 resized.png
+            subprocess.run(
+                [
+                    "convert",
+                    "comic.png",
+                    "-resize",
+                    "800x480",
+                    "-background",
+                    "white",
+                    "-gravity",
+                    "center",
+                    "-extent",
+                    "800x480",
+                    "resized.png",
+                ]
+            )
+            EInkDisplayDriver.display_image()
+
+
 if __name__ == "__main__":
-    try:
-        with open("meta.json") as f:
-            meta = json.load(f)
-        displayed_comic_number = meta.get("num", None)
-    except FileNotFoundError:
-        displayed_comic_number = None
-
-    latest_comic_info_json = get_comic_info_json()
-
-    latest_comic_number = latest_comic_info_json.get("num", None)
-
-    if latest_comic_number is None or latest_comic_number == displayed_comic_number:
-        print("No new comic to display")
-    else:
-        download_comic()
-        # Run convert comic.png -resize 800x480 -background white -gravity center -extent 800x480 resized.png
-        subprocess.run(
-            [
-                "convert",
-                "comic.png",
-                "-resize",
-                "800x480",
-                "-background",
-                "white",
-                "-gravity",
-                "center",
-                "-extent",
-                "800x480",
-                "resized.png",
-            ]
-        )
-        EInkDisplayDriver.display_image()
+    XkcdClient().download_latest()
